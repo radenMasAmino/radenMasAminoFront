94be6e1f5f23af60d0d1a834568cd2c70290c722
ryansyrl <m.ridwan.riansyah@gmail.com>
2020-12-29 09:00:32 +0700

    routing guard gangguan belajar

--- src/router/index.js
+++ src/router/index.js
@@ -122,7 +122,7 @@ const routes = [
     meta: {
 
       requiresAuth: true,
-      google: false
+      google: true
     }
   },
   // --------------------- bawah non-front --------------------------- //
